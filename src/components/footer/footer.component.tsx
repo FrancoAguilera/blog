5a055bdab1670439a1a838a5ce04d5e5e8b82f61
Franco Aguilera <flaalf@gmail.com>
2022-10-23 23:10:49 -0300

    finish basic styles

--- src/components/footer/footer.component.tsx
+++ src/components/footer/footer.component.tsx
@@ -4,5 +4,25 @@ import "./footer.styles.css";
 type Props = {};
 
 export const Footer = (props: Props) => {
-  return <div className="footer">Footer content</div>;
+  return (
+    <div className="footer">
+      <div className="contact">
+        <div>
+          <a href="https://github.com/FrancoAguilera" target="_blank" rel="noreferrer">
+            [ :_ Github ]
+          </a>
+        </div>
+        <div>
+          <a href="https://twitter.com/Ahsen_Juan" target="_blank" rel="noreferrer">
+            [ @Twitter ]
+          </a>
+        </div>
+        <div>
+          <a href="https://stackoverflow.com/users/2743774/franco-aguilera" target="_blank" rel="noreferrer">
+            [ StackOverflow ]
+          </a>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -10,26 +10,10 @@ export const Header = (props: Props) => {
         <img className="box-shadow" src="/profile.jpg" alt="" />
       </div>
       <div className="header-data">
+        <h2>Franco Aguilera</h2>
         <h3>
           <i>"Psiconauta del inconsciente, yo todo lo entiendo, yo todo lo entiendo..."</i>
         </h3>
-        <div className="contact">
-          <p>
-            <a href="https://github.com/FrancoAguilera" target="_blank" rel="noreferrer">
-              [ :_ Github ]
-            </a>
-          </p>
-          <p>
-            <a href="https://twitter.com/Ahsen_Juan" target="_blank" rel="noreferrer">
-              [ @Twitter ]
-            </a>
-          </p>
-          <p>
-            <a href="https://stackoverflow.com/users/2743774/franco-aguilera" target="_blank" rel="noreferrer">
-              [ StackOverflow ]
-            </a>
-          </p>
-        </div>
       </div>
     </div>
   );
